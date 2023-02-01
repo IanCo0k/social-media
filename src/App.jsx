229faa8f9e294a1055f94bdfff092a34abd6a6c9
Ian Cook <ianscook19@gmail.com>
2023-02-01 13:37:13 -0500

    added login

--- src/App.jsx
+++ src/App.jsx
@@ -4,71 +4,78 @@ import 'firebase/compat/auth';
 import 'firebase/compat/firestore';
 
 
-export default function App() {
+  const App = () => {
 
-  const [username, setUsername] = useState('')
-  const [desc, setDesc] = useState('');
+    const firebaseConfig = {
+      apiKey: "AIzaSyALmTermsnlmGpQ3qtn-sKJ6e61c6umKMI",
+      authDomain: "social-media-d3bb4.firebaseapp.com",
+      projectId: "social-media-d3bb4",
+      storageBucket: "social-media-d3bb4.appspot.com",
+      messagingSenderId: "972015921763",
+      appId: "1:972015921763:web:8bc56365f01e4f8f89a656"
+    };
+  
+    firebase.initializeApp(firebaseConfig);
 
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    const db = firebase.firestore();
-    db.collection('users').add({
-      name: username,
-      desc: desc
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [error, setError] = useState(null);
+
+    const [user, setUser] = useState(null);
+
+    firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        setUser(user);
+        console.log(user);
+      } else {
+        setUser(null);
+      }
     });
-    setUsername('');
-    setEmail('');
+  
+    const handleSignIn = async (e) => {
+      e.preventDefault();
+  
+      try {
+        await firebase.auth().signInWithEmailAndPassword(email, password);
+        console.log('successfully logged in')
+      } catch (error) {
+        setError(error.message);
+      }
+    };
+  
+    return (
+      <div>
+      <form onSubmit={handleSignIn}>
+        <input
+          type="email"
+          placeholder="Email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <button type="submit">Sign In</button>
+        {error && <p>{error}</p>}
+      </form>
+
+      {user ? (
+        <p>Signed in as {user.email}</p>
+      ) : (
+        <p>Not signed in</p>
+      )}
+      </div>
+    );
   };
-
-  const firebaseConfig = {
-    apiKey: "AIzaSyALmTermsnlmGpQ3qtn-sKJ6e61c6umKMI",
-    authDomain: "social-media-d3bb4.firebaseapp.com",
-    projectId: "social-media-d3bb4",
-    storageBucket: "social-media-d3bb4.appspot.com",
-    messagingSenderId: "972015921763",
-    appId: "1:972015921763:web:8bc56365f01e4f8f89a656"
-  };
-
-  firebase.initializeApp(firebaseConfig);
-
-  const [users, setUsers] = useState([]);
-
-  const ref = firebase.firestore().collection('users');
+  
+  export default App;
 
 
-  useEffect(() => {
-    ref.onSnapshot((querySnapshot) => {
-      console.log('Users have been loaded:');
-      let updatedUsers = [];
-      querySnapshot.forEach((doc) => {
-        updatedUsers = [...updatedUsers, doc.data()];
-      });
-      setUsers(updatedUsers);
-    });
-  }, [])
   
   
 
 
-  return (
-    <>
-    <div>
-      {users.map((user, index) => {
-      return (
-        <div key={index}>
-          <p>Name: {user.name}</p>
-          <p>Description: {user.desc}</p>
-        </div>
-      );
-    })}
 
-    </div>
-
-    <form onSubmit={handleSubmit}>
-      <input type="text" placeholder="Name" value={username} onChange={(event) => setUsername(event.target.value)} />
-      <input type="text" placeholder="Description" value={desc} onChange={(event) => setDesc(event.target.value)} />
-      <button type="submit">Sign Up</button>
-    </form>
-    </>
-  )
-}
