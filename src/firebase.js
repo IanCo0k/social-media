d06b3da0975ab02545bca6e64009e26facf419b1
Ian Cook <ianscook19@gmail.com>
2023-02-01 15:55:28 -0500

    fixing it

--- src/firebase.js
+++ src/firebase.js
@@ -2,8 +2,6 @@ import firebase from 'firebase/compat/app';
 import 'firebase/compat/auth';
 import 'firebase/compat/firestore';
 
-firebase.initializeApp(firebaseConfig);
-
 const firebaseConfig = {
     apiKey: "AIzaSyALmTermsnlmGpQ3qtn-sKJ6e61c6umKMI",
     authDomain: "social-media-d3bb4.firebaseapp.com",
@@ -12,5 +10,7 @@ const firebaseConfig = {
     messagingSenderId: "972015921763",
     appId: "1:972015921763:web:8bc56365f01e4f8f89a656"
   };
+
+  firebase.initializeApp(firebaseConfig);
 
   export default firebase;
